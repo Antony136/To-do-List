8cd8acfdf86388404599a45350be2e59e60a3235
Antony <icebox1306@gmail.com>
2025-11-09 20:04:02 +0530

    Connection to db

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const connectDB = require("./config/db");
+const dotenv = require("dotenv");
+
+dotenv.config();
+connectDB();
 
 app.get("/", (req, res) => {
     res.send("Api is running");
