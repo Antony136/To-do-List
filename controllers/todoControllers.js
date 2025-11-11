0ca0abba352cebd490aa4fce9c90427a1ec12998
Antony <icebox1306@gmail.com>
2025-11-11 12:17:26 +0530

    Delete and update tasks

--- controllers/todoControllers.js
+++ controllers/todoControllers.js
@@ -10,26 +10,37 @@ const createTodo = async(req, res) => {
     }
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
 const getTodo = async(req, res) => {
     try{
         const todos = await Todo.find();
         res.status(200).json(todos);
     }catch(error){
-        res.status(500).json({"error": "Cannot get the tasks"});
+        res.status(500).json({ message: "Cannot get the tasks" });
     }
 };
 
+const updateTodo = async(req, res) => {
+    try{
+        const { id } = req.params;
+        const todo = await Todo.findByIdAndUpdate(
+            id,
+            req.body,
+            { new: true }
+        );
+        res.status(200).json(todo);
+    }catch(error){
+        res.status(500).json({ message : "Cannot Update task"});
+    }
+};
 
-module.exports = { createTodo, getTodo };
+const deleteTodo = async(req, res) => {
+    try{
+        const { id } = req.params;
+        const todo = await Todo.findByIdAndDelete(id);
+        res.status(200).json({ message : "Task Deleted Successfully"});
+    }catch(error){
+        res.status(500).json({ message : "Cannot Delete Task"});
+    }
+};
+
+module.exports = { createTodo, getTodo, updateTodo, deleteTodo };
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,8 +1,10 @@
-const { createTodo, getTodo } = require("../controllers/todoControllers");
+const { createTodo, getTodo, updateTodo, deleteTodo } = require("../controllers/todoControllers");
 const express = require("express");
 const router = express.Router();
 
 router.post("/", createTodo);
 router.get("/", getTodo);
+router.put("/:id", updateTodo);
+router.delete("/:id", deleteTodo);
 
 module.exports = router;
